4b6732bcadef3f35ec96522dc19da94e2c6c9bcc
Elvis Gastelum <elvisgastelum@outlook.com>
2020-08-16 01:02:52 -0700

    added installer with shell script

---
+++ coc-install.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+set -o nounset    # error when referencing undefined variable
+set -o errexit    # exit when command fails
+cwd=$(pwd)
+# Install latest lts nodejs
+if [ ! -x "$(command -v node)" ]; then
+  curl --fail -LSs https://install-node.now.sh/lts | sh
+  export PATH="/usr/local/bin/:$PATH"
+  # Or use apt-get
+  # sudo apt-get install nodejs
+fi
+
+echo "
+{
+  \"languageserver\": {
+    \"go\": {
+      \"command\": \"gopls\",
+      \"rootPatterns\": [\"go.mod\"],
+      \"trace.server\": \"verbose\",
+      \"filetypes\": [\"go\"]
+    }
+  },
+  \"eslint.autoFixOnSave\": true,
+  \"eslint.filetypes\": [\"javascript\", \"javascriptreact\", \"typescript\", \"typescriptreact\"],
+  \"prettier.disableSuccessMessage\": true,
+  \"prettier.singleQuote\": true,
+  \"coc.preferences.formatOnSaveFiletypes\": [
+    \"javascript\",
+    \"javascriptreact\",
+    \"typescript\",
+    \"typescriptreact\",
+    \"css\", 
+    \"markdown\"
+  ],
+  \"tsserver.formatOnType\": true,
+  \"emmet.includeLanguages\": {\"vue-html\": \"html\", \"javascript\": \"javascriptreact\"},
+  \"coc.preferences.formatOnType\": true,
+  \"coc.preferences.extensionUpdateCheck\": \"never\"
+}
+" > ~/.config/nvim/coc-settings.json 
+
+
+# Install extensions
+mkdir -p ~/.config/coc/extensions
+cd ~/.config/coc/extensions
+if [ ! -f package.json ]
+then
+    echo '{"dependencies":{}}'> package.json
+fi
+# Change extension names to the extensions you need
+npm install \
+  coc-snippets coc-emmet coc-eslint \
+  coc-html coc-json coc-prettier \
+  coc-tslint coc-tsserver \
+  --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+
+cd $cwd
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Install vim
+if [ ! -x "$(command -v vim)" ]; then
+  echo Installing vim
+  sudo apt update
+  sudo apt install vim 
+fi
+
+# Install neovim
+if [ ! -x "$(command -v nvim)" ]; then
+  echo Installing neovim
+  sudo apt update
+  sudo apt install neovim
+fi
+
+echo Setting up neovim
+echo "
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath=&runtimepath
+source ~/.vimrc
+" > ~/.config/nvim/init.vim
+
+
+echo "\" Call the versioned .vimrc file
+if filereadable(expand(\"~/.vim/config/.vimrc\"))
+	source ~/.vim/config/.vimrc
+endif
+" >> ~/.vimrc
+
+echo Configuration installed
+echo Installing Coc.nvim
+source ~/.vim/coc-install.sh
+echo Openning neovim
+nvim
