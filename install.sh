7afc6e3493fe7472a3877052bcb633b89f9db20f
Elvis Gastelum <elvisgastelum@outlook.com>
2020-08-16 02:07:55 -0700

    [MOD] fix bugs

--- install.sh
+++ install.sh
@@ -29,9 +29,9 @@ echo Installing vim-plug for neovim and vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Neovim (~/.local/share/nvim/site/autoload)
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Neovim 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo "\" Call the versioned .vimrc file
 if filereadable(expand(\"~/.vim/config/.vimrc\"))
