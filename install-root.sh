d61541462f6d4e3d591c6ee3708341dcf9a500cf
Elvis Gastelum <43228550+ElvisGastelum@users.noreply.github.com>
2020-09-14 17:58:49 -0700

    Update install-root.sh

--- install-root.sh
+++ install-root.sh
@@ -1,1 +1,67 @@
+#!/usr/bin/env bash
+#
+# Simple use command
+#   curl https://raw.githubusercontent.com/ElvisGastelum/.vim/master/install-root.sh | bash
+# 
+# * install only for ubuntu/debian
+# * and root user
 
+git clone https://github.com/ElvisGastelum/.vim ~/.vim
+
+cd ~/.vim
+
+# Install vim
+if [ ! -x "$(command -v vim)" ]; then
+  echo Installing vim
+  apt update
+  apt install vim -y
+fi
+
+# Install neovim
+if [ ! -x "$(command -v nvim)" ]; then
+  echo Installing neovim
+  add-apt-repository ppa:neovim-ppa/stable -y
+  apt update
+  apt install neovim -y
+fi
+
+echo Setting up neovim
+mkdir -p ~/.config/nvim
+echo "
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath=&runtimepath
+source ~/.vimrc
+" > ~/.config/nvim/init.vim
+
+
+echo Installing vim-plug for neovim and vim
+# Vim (~/.vim/autoload)
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Neovim 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+echo '
+" Call the versioned .vimrc file
+if filereadable(expand("~/.vim/config/.vimrc"))
+	source ~/.vim/config/.vimrc
+endif
+' > ~/.vimrc
+
+echo Configuration installed
+echo Installing Coc.nvim
+. ~/.vim/coc-install.sh
+
+
+if [ ! -x "$(grep -iRl "alias v" ~/.bashrc)" ]; then
+  echo 'Adding alias v => .bashrc for nvim'
+  echo 'alias v="nvim"' >> ~/.bashrc
+  source ~/.bashrc
+fi
+
+echo Install Plugins
+nvim --headless +PlugInstall +qall
+echo Openning neovim to finish the configuration
+nvim
