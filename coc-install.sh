f044873a19a76ec3c426f7101f792ff29ae0dc72
Elvis Gastelum <43228550+ElvisGastelum@users.noreply.github.com>
2020-09-14 21:34:50 -0700

    Update coc-install.sh

--- coc-install.sh
+++ coc-install.sh
@@ -17,13 +17,9 @@ if [ ! -x "$(command -v yarn)" ]; then
   echo 'Installing yarn package manager'
   
   if [ "$(whoami)" = "root" ]; then
-    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-    echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-    apt-get update && apt-get install yarn -y
+    npm install -g yarn
   else
-    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-    sudo apt-get update && sudo apt-get install yarn -y
+    sudo npm install -g yarn
   fi
 fi
 
